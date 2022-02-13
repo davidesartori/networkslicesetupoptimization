75271024d4a4703faf510f4d1cf0968db5bd6b9e
vagrant <vagrant@comnetsemu>
2022-02-13 22:01:15 +0000

    performance monitoring added

--- network.py
+++ network.py
@@ -14,10 +14,6 @@ import modules.config as config
 import os
 import time
 
-conf = config.get_conf("config/network.conf")
-sleep_time = conf["sleep_time"]    # seconds between client iperf requests
-server_addr_file = conf["server_addr_file"]
-current_server_address = conf["server_address"]
 
 class SimpleTopo(Topo):
     "Simple topology of n hosts attached to a switch."
@@ -34,10 +30,9 @@ class SimpleTopo(Topo):
                 self.addLink(host, switch, bw=2, delay='10ms', loss=1, max_queue_size=1000, use_htb=True)
 
 
-def build():
-    topo = SimpleTopo(n=4)
+def build(net):
     c = RemoteController('c', '0.0.0.0', 6633)
-    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, controller=None)
+
     net.addController(c)
     net.start()
     print("Dumping host connections")
@@ -45,50 +40,85 @@ def build():
     print("Testing network connectivity")
     # net.pingAll()
     h1, h2, h3, h4 = net.get('h1', 'h2', 'h3', 'h4')
-    # h1.cmd("./loop.sh&")
-    h1.cmd('./iperfServer.sh {} &'.format(sleep_time))
-    h2.cmd('./iperfClient.sh {} &'.format(sleep_time))
-    h3.cmd('./iperfClient.sh {} &'.format(sleep_time))
-    h4.cmd('./iperfClient.sh {} &'.format(sleep_time))
+    # h1.cmd('./iperfServer.sh {} &'.format(sleep_time))
+    # h2.cmd('./iperfClient.sh {} &'.format(sleep_time))
+    # h3.cmd('./iperfClient.sh {} &'.format(sleep_time))
+    # h4.cmd('./iperfClient.sh {} &'.format(sleep_time))
     # CLI(net)
 
-    # h1.cmd("taskkill /IM iperfServer.sh")
 
-
-def monitor_migration_request():
-    global current_server_address
+def check_server_migration(server_addr_file):
     address_file = open(server_addr_file)
 
     line = address_file.readline()
+    address_file.close()
+    file_server_address = line.split(";")[0].strip()
+
 
     print("Checking for migration requests...")
 
-    while True:
-        file_server_address = line.split(";")[0].strip()
+    if (current_server_address != file_server_address):
+        print("Service migration from " + current_server_address + " to " + file_server_address)
 
-        if(current_server_address != file_server_address):
-            print("Service migration from " + current_server_address + " to " + file_server_address)
+        # service migration
 
-            current_server_address = file_server_address
-
-        time.sleep(int(sleep_time))
-
-    address_file.close()
+    return file_server_address
 
 
-def write_server_address(status="down"):
+def execute_iperf(hosts, current_server):
+    iperf_string = ""
+
+    for host in hosts:
+        h = net.get(host)
+        s = net.get(current_server)
+
+        s.cmd("iperf -s -p 5566&")
+        iperf_output = h.cmd("iperf -c 10.0.0.1 -p 5566")
+        bandwidth = iperf_output.split("\n")[6].split("  ")[4].split(" ")[0]
+        iperf_string += current_server + ";" + host + ";" + bandwidth + "\n"
+
+    return iperf_string
+    
+
+
+def write_server_address(current_server_address, status="down"):
     file_out = open(server_addr_file, "w")
-
     file_out.write(current_server_address + ";" + status)
+    file_out.close()
 
+
+def write_iperf(path, string):
+    file_out = open(path, "w")
+    file_out.write(string + "\n#\n")
     file_out.close()
 
 
 if __name__ == '__main__':
-    write_server_address()
-    
-    build()
+    conf = config.get_conf("config/network.conf")
+    sleep_time = conf["sleep_time"]  # seconds between client iperf requests
+    server_addr_file = conf["server_addr_file"]
+    current_server_address = conf["server_address"]
+    iperf_file = conf["iperf_file"]
+    hosts = ["h3", "h3"]
+    servers = ["h1", "h4"]
+    current_server = "h" + current_server_address.split(".")[-1]
 
-    write_server_address("up")
+    write_server_address(current_server_address)
 
-    # monitor_migration_request()
+    topo = SimpleTopo(n=4)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, controller=None)
+    build(net)
+
+    write_server_address(current_server_address, "up")
+
+    while True:
+        print("Running iperf to check current server performance")
+        iperf_result = execute_iperf(hosts, current_server)
+        iperf_result = iperf_result[:-1]
+        print("Performance analysis done")
+        write_iperf(iperf_file, iperf_result)
+
+        current_server_address = check_server_migration(server_addr_file)
+        current_server = "h" + current_server_address.split(".")[-1]
+
+        time.sleep(int(sleep_time))
