a9110071253dff27e9a7cb7aa4f24f4d8414c457
DecebalCernei <32218284+DecebalCernei@users.noreply.github.com>
2022-02-14 18:10:23 +0100

    using conf file

--- monitoring.py
+++ monitoring.py
@@ -5,14 +5,18 @@ Description: Implementation of network monitoring
 
 #from re import I
 import time
+import modules.config as config
 
-THRESHOLD = 200 #Kbits/s
-CURRENT_BDW = 0
+CURRENT_BANDWIDTH = 0
 
 
 def monitoring():
-    file_in = open('./log/iperf_log.log', 'r')
-    file_out = open('./log/server.log', 'a')
+    conf = config.get_conf("config/monitoring.conf")
+    threshold = conf["threshold"]
+    server_log = conf["server_addr_file"]
+    iperf_log = conf["iperf_file"]
+    file_in = open(iperf_log, 'r')
+    file_out = open(server_log, 'a')
     Lines = file_in.readlines()
     #{ip_server: [bandwidth, client_counter]}
     servers = {}
@@ -22,23 +26,26 @@ def monitoring():
                 server = list(servers)[0]
                 current_bdw = calculate_avg_bandwidth(servers[server][0], servers[server][1])
                 print(current_bdw)
-                if((current_bdw-CURRENT_BDW+THRESHOLD) < 0):
+                if((current_bdw-CURRENT_BANDWIDTH+threshold) < 0):
                     file_out.write('migrate\n')
                     time.sleep(3)
             else:
                 best_server = find_best_server(servers)
-                CURRENT_BDW = best_server[1]
+                CURRENT_BANDWIDTH = best_server[1]
                 print(best_server[0])
                 file_out.write('{}\n'.format(best_server[0]))
-
+        
         else:
-            params = line.split(';')
-            server_ip = params[0]
-            bandwidth = float(params[2].split('\n')[0])
-            if(server_ip not in servers):
-                servers[server_ip] = [0, 0]
-            servers[server_ip][0] = servers[server_ip][0] + bandwidth #sommiamo le bdw
-            servers[server_ip][1] = servers[server_ip][1] + 1 #aumentiamo il numero di client
+            if(line == '0\n'):
+                pass
+            else:
+                params = line.split(';')
+                server_ip = params[0]
+                bandwidth = float(params[2].split('\n')[0])
+                if(server_ip not in servers):
+                    servers[server_ip] = [0, 0]
+                servers[server_ip][0] = servers[server_ip][0] + bandwidth #sommiamo le bdw
+                servers[server_ip][1] = servers[server_ip][1] + 1 #aumentiamo il numero di client
 
 
 
